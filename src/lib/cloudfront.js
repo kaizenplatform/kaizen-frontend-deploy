434c3c9194be0cd7fe56c6de507a2472efb28ddb
Yoshihide Jimbo <yjimbo@gmail.com>
2017-09-15 17:09:50 +0900

    Run formatting with prettier

--- src/lib/cloudfront.js
+++ src/lib/cloudfront.js
@@ -1,30 +1,30 @@
-'use strict'
+'use strict';
 
-const AWS = require('aws-sdk')
-const recursive = require('recursive-readdir')
-const chalk = require('chalk')
+const AWS = require('aws-sdk');
+const recursive = require('recursive-readdir');
+const chalk = require('chalk');
 
 module.exports = {
-  invalidate: async function (localDir, awsAccessKeyId, awsSecretAccessKey, distributionId, s3Prefix = '') {
+  invalidate: async function(localDir, awsAccessKeyId, awsSecretAccessKey, distributionId, s3Prefix = '') {
     return new Promise((resolve, reject) => {
-      console.log(chalk.cyan('\nInvalidating Cloudfront...'))
+      console.log(chalk.cyan('\nInvalidating Cloudfront...'));
 
       const cloudfront = new AWS.CloudFront({
         accessKeyId: awsAccessKeyId,
-        secretAccessKey: awsSecretAccessKey
-      })
-      const invalidateList = [`${s3Prefix}/`]
-      const dir = localDir.replace(/\/$/, '')
+        secretAccessKey: awsSecretAccessKey,
+      });
+      const invalidateList = [`${s3Prefix}/`];
+      const dir = localDir.replace(/\/$/, '');
 
       recursive(dir, (err, files) => {
         if (err) {
-          console.error(chalk.red(err))
-          process.exit(1)
+          console.error(chalk.red(err));
+          process.exit(1);
         }
         files.forEach(file => {
-          invalidateList.push(file.replace(dir, s3Prefix))
-        })
-      })
+          invalidateList.push(file.replace(dir, s3Prefix));
+        });
+      });
 
       const params = {
         DistributionId: distributionId,
@@ -32,36 +32,36 @@ module.exports = {
           CallerReference: Date.now().toString(),
           Paths: {
             Quantity: invalidateList.length,
-            Items: invalidateList
-          }
-        }
-      }
+            Items: invalidateList,
+          },
+        },
+      };
       cloudfront.createInvalidation(params, (err, data) => {
         if (err) {
-          console.error('creating invalidation error', err.stack)
-          process.exit(1)
+          console.error('creating invalidation error', err.stack);
+          process.exit(1);
         }
-        console.log(chalk.green('Created invalidation successfully.'))
-        console.log(chalk.gray(`  Invalidation ID: ${data.Invalidation.Id}`))
-        console.log(chalk.gray(`  Object path:`))
+        console.log(chalk.green('Created invalidation successfully.'));
+        console.log(chalk.gray(`  Invalidation ID: ${data.Invalidation.Id}`));
+        console.log(chalk.gray(`  Object path:`));
         data.Invalidation.InvalidationBatch.Paths.Items.forEach(path => {
-          console.log(chalk.gray(`    ${path}`))
-        })
-        console.log('Waiting for invalidation completed...')
+          console.log(chalk.gray(`    ${path}`));
+        });
+        console.log('Waiting for invalidation completed...');
         const params = {
           DistributionId: distributionId,
-          Id: data.Invalidation.Id
-        }
+          Id: data.Invalidation.Id,
+        };
         cloudfront.waitFor('invalidationCompleted', params, (err, data) => {
           if (err) {
-            console.error(chalk.red('Wating invalidationCompleted error'), err.stack)
-            reject(err.stack)
-            process.exit(1)
+            console.error(chalk.red('Wating invalidationCompleted error'), err.stack);
+            reject(err.stack);
+            process.exit(1);
           }
-          console.log('✨  Done.')
-          resolve()
-        })
-      })
-    })
-  }
-}
+          console.log('✨  Done.');
+          resolve();
+        });
+      });
+    });
+  },
+};
