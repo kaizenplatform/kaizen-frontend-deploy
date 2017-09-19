d2f607fcb1963b6188d5058cbe92281197509811
Yoshihide Jimbo <yjimbo@gmail.com>
2017-09-19 17:29:38 +0900

    Fix S3 prefix

--- src/lib/cloudfront.js
+++ src/lib/cloudfront.js
@@ -14,52 +14,52 @@ module.exports = {
         secretAccessKey: awsSecretAccessKey,
       });
       const invalidateList = [`${s3Prefix}/`];
-      const dir = localDir.replace(/\/$/, '');
 
-      recursive(dir, (err, files) => {
+      recursive(localDir, (err, files) => {
         if (err) {
           console.error(chalk.red(err));
           process.exit(1);
         }
         files.forEach(file => {
-          invalidateList.push(file.replace(dir, s3Prefix));
+          invalidateList.push(file.replace(localDir, s3Prefix));
         });
-      });
 
-      const params = {
-        DistributionId: distributionId,
-        InvalidationBatch: {
-          CallerReference: Date.now().toString(),
-          Paths: {
-            Quantity: invalidateList.length,
-            Items: invalidateList,
-          },
-        },
-      };
-      cloudfront.createInvalidation(params, (err, data) => {
-        if (err) {
-          console.error('creating invalidation error', err.stack);
-          process.exit(1);
-        }
-        console.log(chalk.green('Created invalidation successfully.'));
-        console.log(chalk.gray(`  Invalidation ID: ${data.Invalidation.Id}`));
-        console.log(chalk.gray(`  Object path:`));
-        data.Invalidation.InvalidationBatch.Paths.Items.forEach(path => {
-          console.log(chalk.gray(`    ${path}`));
-        });
-        console.log('Waiting for invalidation completed...');
         const params = {
           DistributionId: distributionId,
-          Id: data.Invalidation.Id,
+          InvalidationBatch: {
+            CallerReference: Date.now().toString(),
+            Paths: {
+              Quantity: invalidateList.length,
+              Items: invalidateList,
+            },
+          },
         };
-        cloudfront.waitFor('invalidationCompleted', params, (err, data) => {
+
+        cloudfront.createInvalidation(params, (err, data) => {
           if (err) {
-            console.error(chalk.red('Wating invalidationCompleted error'), err.stack);
-            reject(err.stack);
+            console.error('creating invalidation error', err.stack);
             process.exit(1);
           }
-          console.log('✨  Done.');
-          resolve();
+          console.log(chalk.green('Created invalidation successfully.'));
+          console.log(chalk.gray(`  Invalidation ID: ${data.Invalidation.Id}`));
+          console.log(chalk.gray(`  Object path:`));
+          data.Invalidation.InvalidationBatch.Paths.Items.forEach(path => {
+            console.log(chalk.gray(`    ${path}`));
+          });
+          console.log('Waiting for invalidation completed...');
+          const params = {
+            DistributionId: distributionId,
+            Id: data.Invalidation.Id,
+          };
+          cloudfront.waitFor('invalidationCompleted', params, (err, data) => {
+            if (err) {
+              console.error(chalk.red('Wating invalidationCompleted error'), err.stack);
+              reject(err.stack);
+              process.exit(1);
+            }
+            console.log('✨  Done.');
+            resolve();
+          });
         });
       });
     });
